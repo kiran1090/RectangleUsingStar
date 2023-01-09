caa1365ad7353c921bf5c010e47e35aeae14a7f3
kiran_a <iamkiranashok@gmail.com>
2023-01-09 14:31:05 +0530

    first commit

--- src/SquarePattern.java
+++ src/SquarePattern.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class SquarePattern {
+import java.util.Scanner;
+public class SquarePattern{
+    public static void main(String[] args){
+        int i, j, my_input;
+        Scanner my_scanner = new Scanner(System.in);
+        System.out.print("Enter the length of a side : ");
+        my_input = my_scanner.nextInt();
+        System.out.println("The square pattern : ");
+        for(i = 1; i <= my_input; i++){
+            for(j = 1; j <= my_input; j++){
+                System.out.print("*");
+            }
+            System.out.print("");
+        }
+    }
 }
